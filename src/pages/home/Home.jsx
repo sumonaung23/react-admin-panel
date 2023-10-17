f7ccdf30ece2eb2b0ff84eefa3e62dba70bb437c
sumonaung23 <sumonaung575@gmail.com>
2023-10-17 23:55:49 +0630

    added components in Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
     return (
         <div className='home'>
-            <h1 className="title">Sumon Dev</h1>
+            <Sidebar />
+            <div className="homeContainer">
+                <Navbar />
+                home container
+            </div>
         </div>
     )
 }
