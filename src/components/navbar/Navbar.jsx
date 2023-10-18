8df9d4a3fe381189a760a13553b2d5ddf45f7289
sumonaung23 <sumonaung575@gmail.com>
2023-10-18 23:58:54 +0630

    added counter value

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,9 +29,11 @@ const Navbar = () => {
                     </div>
                     <div className="item">
                         <NotificationsNoneOutlinedIcon className='icon' />
+                        <div className="counter">1</div>
                     </div>
                     <div className="item">
                         <ChatBubbleOutlineOutlinedIcon className='icon' />
+                        <div className="counter">2 </div>
                     </div>
                     <div className="item">
                         <ListOutlinedIcon className='icon' />
