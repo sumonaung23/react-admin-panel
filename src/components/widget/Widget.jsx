fb6ebaa9208d241390088391f87645a670a1895a
sumonaung23 <sumonaung575@gmail.com>
2023-10-18 23:59:41 +0630

    created widget

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Widget.scss'
+
+const widget = () => {
+    return (
+        <div className='widget'>
+            <div className="left">left</div>
+            <div className="right">right</div>
+        </div>
+    )
+}
+
+export default widget
