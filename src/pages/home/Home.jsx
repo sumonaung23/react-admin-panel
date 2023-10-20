e0563d52bd103957bb21b3dd883f9aad6b5c2ccb
sumonaung23 <sumonaung575@gmail.com>
2023-10-20 23:47:41 +0630

    added listContainer section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -22,6 +22,9 @@ const Home = () => {
                     <Featured />
                     <Chart />
                 </div>
+                <div className="listContainer">
+                    <div className="listTitle">Latest Transactions</div>
+                </div>
             </div>
         </div>
     )
