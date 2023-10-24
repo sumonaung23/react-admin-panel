6e493c7b8ba95479a671cf89cd7ef6df67df47e7
sumonaung23 <sumonaung575@gmail.com>
2023-10-24 23:33:58 +0630

    created user list with Sibebar, and inside the listContainer added Navbar and Datatable

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Datatable from '../../components/datatable/Datatable'
+
+import './list.scss'
 
 const List = () => {
     return (
-        <div>List</div>
+        <div className='list'>
+            <Sidebar />
+            <div className="listContainer">
+                <Navbar />
+                <Datatable />
+            </div>
+        </div>
     )
 }
 
