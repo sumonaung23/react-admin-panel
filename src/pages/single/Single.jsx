59926e6188083272218147e08a0cd11a845392ca
sumonaung23 <sumonaung575@gmail.com>
2023-10-25 23:59:54 +0630

    structured single page with Navbar, chart and list

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import './single.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Chart from '../../components/chart/Chart'
+import List from '../../components/table/Table'
 
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className='single'>
+            <Sidebar />
+            <div className="singleContainer">
+                <Navbar />
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img src="https://images.unsplash.com/photo-1678346496584-e81410d1e697?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ3fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="woman" className="itemImg" />
+                            <div className="details">
+                                <h1 className="itemTitle">Jane Doe</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemValue">janedoe@gmail.com</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemValue">+1 2313 234 342</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemValue">Elton St. 234 Garden Yd. NewYork</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemValue">USA</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Chart aspect={ 3 / 1 } title="User Spending ( Last 6 Mohths)" />
+                    </div>
+                </div>
+                <div className="bottom">
+                    <h1 className="title">Last Transactions</h1>
+                    <List />
+                </div>
+            </div>
+        </div>
     )
 }
 
