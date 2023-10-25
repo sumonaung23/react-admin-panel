a0b2e6086061d9ae10a1afa3e088bef8443e88c3
sumonaung23 <sumonaung575@gmail.com>
2023-10-25 23:58:53 +0630

    modified wtih props

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './chart.scss'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const data = [
     { name: "January", Total: 1200 },
@@ -11,11 +11,11 @@ const data = [
     { name: "June", Total: 1700 }
     ];
     
-const Chart = () => {
+const Chart = ({aspect, title}) => {
     return (
         <div className='chart'>
-            <div className="title">Last 6 Months (Revenue)</div>
-            <ResponsiveContainer width="100%" aspect={2 / 1}>
+            <div className="title">{title}</div>
+            <ResponsiveContainer width="100%" aspect={aspect}>
                 <AreaChart width={730} height={250} data={data}
                     margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                     <defs>
