b9c8c397692ab3121aed95f6d7e2ca14ac65c7fe
sumonaung23 <sumonaung575@gmail.com>
2023-10-27 16:25:33 +0630

    added context and reducer files for color-theme

---
+++ src/context/darkModeContext.js
@@ -0,0 +1,19 @@
+import { useReducer, createContext } from "react";
+import DarkModeReducer from "./darkModeReducer";
+
+const INITIAL_STATE = {
+    darkMode: false,
+};
+
+export const DarkModeContext = createContext(INITIAL_STATE);
+
+export const DarkModeContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
+
+    return (
+        <DarkModeContext.Provider value={{darkMode: state.darkMode, dispatch}}>
+            {children}
+        </DarkModeContext.Provider>
+    );
+};
+
